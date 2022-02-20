5f2d7fc7905f74895200251a115907080356616e
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-20 16:01:44 +0800

    array map function updated

--- chaining.js
+++ chaining.js
@@ -1,2 +1,38 @@
 // declare variable based on the name of an object property
 const myObject = {x: 2, y: 50, z: 600, a: 25, b: 68};
+const { x, b} = myObject;
+// console.log('myObject p.', myObject?.p?.q);
+
+// destructuring array
+const [p, q] = [45, 37, 91, 86];
+// console.log(p,q);
+
+
+const [best, faltu] = ['momotaj', 'poroshi'];
+// console.log(best, faltu);
+
+const { sky, color, money } = {sky: 'blue', soil: 'mati', color: 'red', money: 500 };
+// console.log(sky, color, money)
+
+// chaining
+const company = {
+    name: 'GP',
+    ceo: {
+        id: 1,
+        name: 'ajmol',
+        food: 'fucka'
+    },
+    web: {
+        work: 'website development',
+        employee: 22,
+        framework: 'react',
+        tech: {
+            first: 'html',
+            second: 'css',
+            third: 'js'
+        }
+    }
+}
+
+console.log(company?.web?.tech?.third);
+console.log(company?.backend?.tech.third);
